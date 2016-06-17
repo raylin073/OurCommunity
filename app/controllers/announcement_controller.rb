6dbc83a558a4a5d04daa98eb182cdbaa72557823
Ray Lin <ray.lin@titansoft.com.sg>
2016-06-17 12:35:11 +0800

    announcement -> announcements

--- app/controllers/announcement_controller.rb
+++
@@ -1,2 +0,0 @@
-class AnnouncementController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
     root 'main#index'
 
 
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
