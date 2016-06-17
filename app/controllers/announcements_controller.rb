e645fddb33a02deeaed2cd1d3ef83d51dbb55eb5
Ray Lin <ray.lin@titansoft.com.sg>
2016-06-17 14:49:30 +0800

    able to create announcement and show announcements in the root page

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,9 +1,15 @@
 class AnnouncementsController < ApplicationController
-    def index
-    end
     def create
+        @announcement = Announcement.new(announcement_params)
+        @announcement.save
         render 'index'
     end
+    def index
+    end
     def new
+    end
+    private
+    def announcement_params
+        params.require(:announcement).permit(:title, :announcer, :detail)
     end
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,5 @@
 class MainController < ApplicationController
   def index
+    @announcements = Announcement.all
   end
 end
