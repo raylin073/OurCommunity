3e0b207e4c1ce329f6d5b10b4c1665e807bd7b2c
Stanly Lau <stanly@odd-e.com>
2016-06-17 15:01:42 +0800

    Added factorygirl

--- spec/controllers/announcements_controller_spec.rb
+++ spec/controllers/announcements_controller_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe AnnouncementsController, type: :controller do
-
+  it 'index' do
+    
+  end
 end
---
+++ spec/factories/announcements.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :announcement do
+      title "Meatballs dropped from the sky"
+      detail "A local scientist is often regarded as a failure until he invents a machine that can make food fall from the sky."
+      announcer "Earl Devereaux"
+  end
+
+end
